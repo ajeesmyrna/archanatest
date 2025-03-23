e84c373ea49154104889f4c63945b6492928c71a
ajeesmyrna <ajeesmyrna@outlook.com>
2025-03-22 21:59:43 -0400

    terraform destroy when main bracnh push

--- main.tf
+++ main.tf
@@ -1,5 +1,4 @@
 module "resourcegrp" {
-description = "Module for creating resource groups"
 source = "./modules/resourcegrp"
 for_each = var.resource_group_name
 resource_group_name = each.value.name
