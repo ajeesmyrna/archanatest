d6b246c92a6ccbeac953eeef709dcfd7b0a81cc0
ajeesmyrna <ajeesmyrna@outlook.com>
2025-03-22 21:54:15 -0400

    commit from main

--- main.tf
+++ main.tf
@@ -4,14 +4,3 @@ for_each = var.resource_group_name
 resource_group_name = each.value.name
 location            = var.location
 }
-
-/*
-output "resource_group_name_from_module" {
-  description = "The name of the resource group from the resourcegrp module"
-  value       = module.resourcegrp.resource_group_name
-}
-
-output "resource_group_location_from_module" {
-  description = "The location of the resource group from the resourcegrp module"
-  value       = module.resourcegrp.resource_group_location
-}*/
