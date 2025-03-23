7793eb769bfad6f8c137a1ed64ae870044a9c5d5
ajeesmyrna <ajeesmyrna@outlook.com>
2025-03-23 16:13:34 -0400

    depends on addded for vnet resource block

--- modules/vnetandsubnets/main.tf
+++ modules/vnetandsubnets/main.tf
@@ -1,8 +1,19 @@
+# Create a resource group
+resource "azurerm_resource_group" "vnet-rg" {
+  name     = "${var.resource_group_name}RG"
+  location = var.location
+}
+
 resource "azurerm_virtual_network" "archanaprod-vnet" {
   name                = var.name
   location            = var.location
   resource_group_name = var.resource_group_name
   address_space       = var.address_space
+ # Ensure this depends on the resource group
+  depends_on = [
+    azurerm_resource_group.vnet-rg
+  ]
+
 }
 
 resource "azurerm_subnet" "subnets" {
@@ -10,4 +21,10 @@ resource "azurerm_subnet" "subnets" {
   virtual_network_name = azurerm_virtual_network.archanaprod-vnet.name
   name                 = var.subnet_name
   address_prefixes     = var.subnet_address_prefixes
+
+  # Ensure this depends on the resource group
+  depends_on = [
+    azurerm_resource_group.vnet-rg
+    azurerm_virtual_network.archanaprod-vnet
+  ]
   }
