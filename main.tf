9045171f00cd10dbf0b0ce7cbba429ce333bf984
ajeesmyrna <ajeesmyrna@outlook.com>
2025-03-24 17:42:11 -0400

    commiting archananonprod-vm1

--- main.tf
+++ main.tf
@@ -39,14 +39,14 @@ module "archanaprod-vm2" {
 }
 
 
-/*module "vm_nonprod" {
+module "archananonprod-vm1" {
   source              = "./modules/virtualmachine"
-  vm_name             = "nonprod-vm"
+  vm_name             = "archananonprod-vm1"
   location            = module.resourcegrp["nonprod"].resource_group_location
   resource_group_name = module.resourcegrp["nonprod"].resource_group_name
-  subnet_id           = module.vnetandsubnets.subnets["subnet2"].id
+  subnet_id           = module.vnetandsubnets.subnets["nonprod"]
   vm_size             = "Standard_B2s"
   admin_username      = "adminuser"
   admin_password      = "P@ssw0rd123!"
   create_public_ip    = false
-}*/
+}
