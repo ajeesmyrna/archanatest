beef237ddec943593c8dc4ab2844f68b4c61cd80
ajeesmyrna <ajeesmyrna@outlook.com>
2025-03-25 16:27:43 -0400

    deleted local modules

--- modules/resourcegrp/variable.tf
+++
@@ -1,8 +0,0 @@
-variable "resource_group_name" {
-    description = "resource group name for archanaprod"
-    type = string
-}
-variable "location" {
-    description = "resoruce group location"
-    type        = string
-}
