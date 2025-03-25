818b7e7e9860fa9e4f2237881d8afb1c951a7680
ajeesmyrna <ajeesmyrna@outlook.com>
2025-03-25 16:52:32 -0400

    removing .terraform directory

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "resourcegrp" {
   #source             = "./modules/resourcegrp"
-  source              = "git::https://github.com/ajeesmyrna/archanaprod.git//modules/resourcegrp?ref=mainmarch24"
+  source              = "git::https://github.com/ajeesmyrna/archanaprod.git//modules/resourcegrp?ref=main"
   for_each            = var.resource_group_name
   resource_group_name = each.value.name
   location            = var.location
@@ -8,45 +8,17 @@ module "resourcegrp" {
 
 module "vnetandsubnets" {
   #source              = "./modules/vnetandsubnets"
-  source              = "git::https://github.com/ajeesmyrna/archanaprod.git//modules/vnetandsubnets?ref=mainmarch24"
+  source              = "git::https://github.com/ajeesmyrna/archanaprod.git//modules/vnetandsubnets?ref=main"
   name                = var.vnet_name
-  location            = module.resourcegrp["vnet"].resource_group_location
-  resource_group_name = module.resourcegrp["vnet"].resource_group_name
+  location            = module.resourcegrp["nonprod"].resource_group_location
+  resource_group_name = module.resourcegrp["nonprod"].resource_group_name
   address_space       = var.address_space
   subnets             = var.subnets
 }
 
-module "archanaprod-vm1" {
-  #source              = "./modules/virtualmachine"
-  source              = "git::https://github.com/ajeesmyrna/archanaprod.git//modules/virtualmachine?ref=mainmarch24"
-  vm_name             = "archanaprod-vm1"
-  location            = module.resourcegrp["prod"].resource_group_location
-  resource_group_name = module.resourcegrp["prod"].resource_group_name
-  subnet_id           = module.vnetandsubnets.subnets["prod"]
-  vm_size             = "Standard_B2s"
-  admin_username      = "adminuser"
-  admin_password      = "P@ssw0rd123!"
-  create_public_ip    = false
-}
-
-/*
-module "archanaprod-vm2" {
-  #source              = "./modules/virtualmachine"
-  source              = "git::https://github.com/ajeesmyrna/archanaprod.git//modules/virtualmachine?ref=mainmarch24"
-  vm_name             = "archanaprod-vm2"
-  location            = module.resourcegrp["prod"].resource_group_location
-  resource_group_name = module.resourcegrp["prod"].resource_group_name
-  subnet_id           = module.vnetandsubnets.subnets["prod"]
-  vm_size             = "Standard_B2s"
-  admin_username      = "adminuser"
-  admin_password      = "P@ssw0rd123!"
-  create_public_ip    = false
-}
-*/
-
 module "archananonprod-vm1" {
   #source              = "./modules/virtualmachine"
-  source              = "git::https://github.com/ajeesmyrna/archanaprod.git//modules/virtualmachine?ref=mainmarch24"
+  source              = "git::https://github.com/ajeesmyrna/archanaprod.git//modules/virtualmachine?ref=main"
   vm_name             = "archananonprod-vm1"
   location            = module.resourcegrp["nonprod"].resource_group_location
   resource_group_name = module.resourcegrp["nonprod"].resource_group_name
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,7 +1,4 @@
 resource_group_name = {
-  prod = {
-    name = "prod-rg"
-  }
   nonprod = {
     name = "nonprod-rg"
   }
@@ -17,10 +14,6 @@ vnet_name = "archanaprod-vnet"
 address_space = ["10.30.0.0/16"]
 
 subnets = {
-  prod = {
-    name            = "prod"
-    address_prefixes = ["10.30.10.0/24"]
-  }
   nonprod = {
     name            = "nonprod"
     address_prefixes = ["10.30.20.0/24"]
